75d08755b727618aa8181968e94dd86c7b12484e
Salik Ansari <salik.ansari6@gmail.com>
2021-08-19 16:38:15 +0530

    random

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const path = require("path");
 const app = express();
 
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
@@ -15,7 +17,9 @@ mongoose.connect(
   }
 );
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log("server running on port 4000");
 });
 
@@ -28,3 +32,14 @@ app.get("/product/:id", async (req, res) => {
   const product = await Product.find({ id: req.params.id });
   res.json(product);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get(["*"], (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("server is working");
+  });
+}
